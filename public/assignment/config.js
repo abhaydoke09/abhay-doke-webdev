9dc1b4580e86d4d250a43d006ae24f88a25db1cd
Abhay Doke <abhaydoke09@gmail.com>
2017-01-09 11:57:07 -0500

    added login controller, create login function, added error message for login, bound login page model with controller view model

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -8,15 +8,27 @@
 
     function config($routeProvider){
         $routeProvider
+            .when("/",{
+                templateUrl:"view/home.html"
+            })
             .when("/login",{
-                templateUrl:"view/user/login.view.client.html"
+                templateUrl:"view/user/login.view.client.html",
+                controller:"LoginController",
+                controllerAs:"model"
             })
             .when("/register",{
                 templateUrl:"view/user/register.view.client.html"
             })
             .when("/profile",{
                 templateUrl:"view/user/profile.view.client.html"
+            })
+            .when("/website-list",{
+                templateUrl:"view/website/website-list.view.client.html"
+            })
+            .otherwise({
+                redirectTo:"/login"
             });
+
 
     }
 })();
